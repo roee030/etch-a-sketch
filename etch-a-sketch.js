5764458d9880b906202771bd6b35aab1811eccde
roee030 <roee.angel93@gmail.com>
2020-08-30 20:24:04 +0300

    keydown listener event

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -14,3 +14,49 @@ let y = Math.floor(Math.random() * height);
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+ctx.beginPath(); // start the drawing
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+function draw({ key }) {
+	// increment the hue
+	hue += 1;
+	console.log(hue);
+	ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+	console.log(key);
+	// start the path
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+	// move our x and y values depending on what the user did
+	switch (key) {
+		case 'ArrowUp':
+			y -= MOVE_AMOUNT;
+			break;
+		case 'ArrowRight':
+			x += MOVE_AMOUNT;
+			break;
+		case 'ArrowDown':
+			y += MOVE_AMOUNT;
+			break;
+		case 'ArrowLeft':
+			x -= MOVE_AMOUNT;
+			break;
+		default:
+			break;
+	}
+	ctx.lineTo(x, y);
+	ctx.stroke();
+}
+// write a handler for the keys
+
+function handleKey(e) {
+	if (e.key.includes('Arrow')) {
+		e.preventDefault();
+		draw({ key: e.key });
+	}
+}
+
+window.addEventListener('keydown', handleKey);
