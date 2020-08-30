8e4e66de145b89632232f02adb873e6c42cb568c
roee030 <roee.angel93@gmail.com>
2020-08-30 20:33:10 +0300

    done

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
-const MOVE_AMOUNT = 50;
+const MOVE_AMOUNT = 10;
 // Setup our canvas for drawing
 // make a variable called height and width from the same properties on our canvas.
 const { width, height } = canvas;
@@ -59,4 +59,19 @@ function handleKey(e) {
 	}
 }
 
+function clearCanvas() {
+	canvas.classList.add('shake');
+	ctx.clearRect(0, 0, width, height);
+	canvas.addEventListener(
+		'animationend',
+		function () {
+			console.log('Done the shake!');
+			canvas.classList.remove('shake');
+		},
+		{ once: true }
+	);
+}
+
+// listen for arrow keys
 window.addEventListener('keydown', handleKey);
+shakebutton.addEventListener('click', clearCanvas);
